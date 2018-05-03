a6efdcae1532d36e5c2390bd141b448a3d7d9f0e
uolly <38909267+uolly@users.noreply.github.com>
2018-05-03 16:57:14 +0200

    Add files via upload

---
+++ pdftables_extractor/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace pdftable_extractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.buttonPDF2table = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // buttonPDF2table
+            // 
+            this.buttonPDF2table.Location = new System.Drawing.Point(58, 80);
+            this.buttonPDF2table.Name = "buttonPDF2table";
+            this.buttonPDF2table.Size = new System.Drawing.Size(171, 40);
+            this.buttonPDF2table.TabIndex = 0;
+            this.buttonPDF2table.Text = "Convert PDFTables";
+            this.buttonPDF2table.UseVisualStyleBackColor = true;
+            this.buttonPDF2table.Click += new System.EventHandler(this.buttonPDF2table_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.buttonPDF2table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button buttonPDF2table;
+    }
+}
+
---
+++ pdftables_extractor/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Http;
+using System.IO;
+
+namespace pdftable_extractor
+{
+    public partial class Form1 : Form
+    {
+        const string format = "xlsx-single";
+        const string apiKey = " "; //put here your api key from pdftables
+        const string uploadURL = "https://pdftables.com/api?key=" + apiKey + "&format=" + format;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void startPDFImport(string pdfFilename)
+        {
+            var task =  PDFToExcel(pdfFilename, @"e:\test.xlsx"); //put here your excel/csv/xml path
+            task.Wait();
+            MessageBox.Show("Import finished. Exit Code: " + ((int)task.Result).ToString()); //this message box is just to show that process is terminated
+
+            // Do something with your new xlsx, xml or csv file
+        }
+
+        static async Task<HttpStatusCode> PDFToExcel(string pdfFilename, string xlsxFilename)
+        {
+            using (var f = System.IO.File.OpenRead(pdfFilename))
+            {
+                var client = new HttpClient();
+                var mpcontent = new MultipartFormDataContent();
+                mpcontent.Add(new StreamContent(f));
+
+                using (var response = await client.PostAsync(uploadURL, mpcontent).ConfigureAwait(false))
+                {
+                    if ((int)response.StatusCode == 200)
+                    {
+                        using (
+                            Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                            stream = File.Create(xlsxFilename))
+                        {
+                            await contentStream.CopyToAsync(stream);
+                        }
+                    }
+                    return response.StatusCode;
+                }
+            }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void buttonPDF2table_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog pdfDialog = new OpenFileDialog();
+            pdfDialog.Filter = "PDF files (*.pdf) | *.pdf";
+            pdfDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //choos your initial directory
+            pdfDialog.Title = "Import PDF file";
+
+            if (pdfDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                startPDFImport(pdfDialog.FileName);
+            }
+        }
+    }
+}
